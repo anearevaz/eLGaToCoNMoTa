65bc8d6dc64ed7ef5c52c6cfa8780d7588dea21b
AAV <anearevaz@gmail.com>
2017-02-20 10:45:16 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/elgatoconmota/eLGaToCoNMoTa_File.java
@@ -0,0 +1,23 @@
+package com.example.android.elgatoconmota;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class eLGaToCoNMoTa_File extends AppCompatActivity {
+
+    @Override
+
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_elgatoconmota);
+    }
+}
+
+
+
+
+
+}
